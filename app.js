b4b2e548e51ebf7de99e26b5d4d25a8ea2865e63
Facundo Saracco <facundosaracco@gmail.com>
2021-11-10 09:58:16 +0100

    done

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const path = require("path")
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")))
+
+app.all("/", (request, response)=>{
+    response.sendFile(path.join(__dirname, "views", "index.html"))
+})
+
+app.all("/about", (request, response)=>{
+    response.sendFile(path.join(__dirname, "views", "about.html"))
+})
+
+app.all("/works", (request, response)=>{
+    response.sendFile(path.join(__dirname, "views", "works.html"))
+})
+
+app.all("/gallery", (request, response)=>{
+    response.sendFile(path.join(__dirname, "views", "gallery.html"))
+})
+
+app.listen(3000, ()=>{
+    console.log("Yey APP running")
+})
